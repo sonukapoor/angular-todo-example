ccb9083bd9114b63e139e1f85fa653e8c36c9e9a
Greg Wilson <greg.wilson@rangle.io>
2017-04-11 10:02:09 -0400

    Titlecase pipe

---
+++ src/app/titlecase.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'titlecase'
+})
+export class TitlecasePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return value.charAt(0).toUpperCase()
+         + value.substr(1).toLowerCase();
+  }
+}
